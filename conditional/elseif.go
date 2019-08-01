f658a82e095dd741eaccb0c76313bd0ceec21e55
Sathish Vasu <satvasu@publicisgroupe.net>
2019-08-01 17:48:39 +0530

    Added new collections

---
+++ conditional/elseif.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main(){
+	num:= 0.12
+
+	if num == math.Pow(math.Sqrt(num),2){
+		fmt.Println("These are same")
+	}else {
+		fmt.Println("These are different")
+	}
+}
